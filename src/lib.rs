1c64669b93192440071ba0726008aa0b083316c8
sanfusu <sanfusu@outlook.com>
2020-11-17 22:43:34 +0800

    add doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
 extern crate guid_proc;
 pub use guid_proc::*;
 
+/// 全局唯一标识符 (RFC 4122)
 #[derive(Debug)]
+#[repr(C)]
 pub struct Guid {
+    /// TimeLow: 时间戳的低字段
     pub data1: u32,
+    /// TimeMid: 时间戳的中间字段
     pub data2: u16,
+    /// 时间戳的高位字段和版本号（复用）
     pub data3: u16,
+    /// 第一个字节为时钟序列的高位字段；
+    /// 第二个字节为时钟序列的低位字段；
+    /// 剩余 6 个字节为唯一的节点标识，
+    /// 可以是 IEEE 802 地址，或者用于加密的随机数。
     pub data4: [u8; 8],
 }
 
@@ -13,8 +22,7 @@ pub struct Guid {
 mod test {
     use super::Guid;
     use guid_proc::guid;
-    extern crate guid_proc;
-
+    
     #[test]
     fn test() {
         let b = guid! {72631e54-78a4-11d0-bcf7-00aa00b7b32a};
