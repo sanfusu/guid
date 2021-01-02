6c9864a1c8fdd8bf7f2ef57af9b5a061b5595f04
sanfusu <sanfusu@foxmail.com>
2021-01-03 03:06:15 +0800

    refactor;remove pest dep

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,12 @@
 use std::convert::TryInto;
 
-use pest::Parser;
-
-// #[macro_use]
-// extern crate guid_proc;
 #[macro_use]
-extern crate pest_derive;
-extern crate pest;
+extern crate layout;
+
 /// 全局唯一标识符 (RFC 4122)
 #[derive(Debug, PartialEq, Eq)]
 #[repr(C)]
+#[derive(Layout)]
 pub struct Guid {
     /// TimeLow: 时间戳的低字段
     pub data1: u32,
@@ -23,56 +20,26 @@ pub struct Guid {
     /// 可以是 IEEE 802 地址，或者用于加密的随机数。
     pub data4: [u8; 8],
 }
-pub(crate) mod guid_pest {
-    #[derive(Parser)]
-    #[grammar = "guid.pest"]
-    pub(crate) struct Guid;
-}
 
-impl std::convert::From<String> for Guid {
-    fn from(v: String) -> Self {
-        let mut token_str = v.replace(' ', "");
-        token_str = token_str.replace('"', "");
-        let guid_parsed = guid_pest::Guid::parse(guid_pest::Rule::guid, &token_str)
-            .unwrap()
-            .next()
-            .unwrap();
+impl std::convert::TryFrom<String> for Guid {
+    type Error = &'static str;
 
-        let mut data1: u32 = 0;
-        let mut data2: u16 = 0;
-        let mut data3: u16 = 0;
-        let mut data4: Vec<u8> = Vec::new();
-        for part in guid_parsed.into_inner() {
-            match part.as_rule() {
-                guid_pest::Rule::part1_u32 => {
-                    data1 = u32::from_str_radix(part.as_str(), 16).unwrap();
-                }
-                guid_pest::Rule::part2_u16 => {
-                    data2 = u16::from_str_radix(part.as_str(), 16).unwrap();
-                }
-                guid_pest::Rule::part3_u16 => {
-                    data3 = u16::from_str_radix(part.as_str(), 16).unwrap();
-                }
-                guid_pest::Rule::part4_u8_8 => {
-                    for byte in part.into_inner() {
-                        data4.push(u8::from_str_radix(byte.as_str(), 16).unwrap());
-                    }
-                }
-                _ => {}
-            }
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        let mut value = value.replace(' ', "");
+        value = value.replace('"', "");
+        value.retain(|c| c != '-');
+        if value.len() != std::mem::size_of::<Guid>() * 2 {
+            return Err("Invalid format");
         }
-        Guid {
-            data1,
-            data2,
-            data3,
-            data4: data4.as_slice().try_into().unwrap(),
-        }
-    }
-}
-
-impl syn::parse::Parse for Guid {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        Ok(input.to_string().into())
+        let integer = u128::from_str_radix(&value, 16)
+            .or(Err("Invalid format"))?
+            .to_be_bytes();
+        Ok(Guid {
+            data1: u32::from_be_bytes(integer[GuidLayout::data1()].try_into().unwrap()).to_le(),
+            data2: u16::from_be_bytes(integer[GuidLayout::data2()].try_into().unwrap()).to_le(),
+            data3: u16::from_be_bytes(integer[GuidLayout::data3()].try_into().unwrap()).to_le(),
+            data4: integer[GuidLayout::data4()].try_into().unwrap(),
+        })
     }
 }
 
@@ -83,7 +50,7 @@ impl quote::ToTokens for Guid {
         let data3 = self.data3;
         let data4 = self.data4;
         let out = quote::quote! {
-            Guid {
+            guid::Guid {
                 data1:#data1,
                 data2:#data2,
                 data3:#data3,
