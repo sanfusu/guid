67a81af7cb921e05ddd57b4dcdc3b240f90ed19b
sanfusu <sanfusu@foxmail.com>
2021-01-05 12:22:44 +0800

    roubust and parse impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::convert::TryInto;
+use std::{array::TryFromSliceError, convert::TryInto, fmt::Display, num::ParseIntError};
 
 #[macro_use]
 extern crate layout;
@@ -58,5 +58,127 @@ impl quote::ToTokens for Guid {
             }
         };
         *tokens = out;
+    }
+}
+
+impl std::str::FromStr for Guid {
+    type Err = ParseGuidError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut s = s.replace(' ', "");
+        s = s.replace('"', "");
+        s.retain(|c| c != '-');
+        if s.len() != std::mem::size_of::<Guid>() * 2 {
+            return Err(ParseGuidError::default());
+        }
+        let integer = u128::from_str_radix(&s, 16)
+            .map_err(ParseGuidError::parse_int_err)?
+            .to_be_bytes();
+        Ok(Guid {
+            data1: u32::from_be_bytes(
+                integer[GuidLayout::data1()]
+                    .try_into()
+                    .map_err(ParseGuidError::try_from_slice_err)?,
+            )
+            .to_le(),
+            data2: u16::from_be_bytes(
+                integer[GuidLayout::data2()]
+                    .try_into()
+                    .map_err(ParseGuidError::try_from_slice_err)?,
+            )
+            .to_le(),
+            data3: u16::from_be_bytes(
+                integer[GuidLayout::data3()]
+                    .try_into()
+                    .map_err(ParseGuidError::try_from_slice_err)?,
+            )
+            .to_le(),
+            data4: integer[GuidLayout::data4()]
+                .try_into()
+                .map_err(ParseGuidError::try_from_slice_err)?,
+        })
+    }
+}
+
+#[derive(Debug)]
+enum ParseGuidErrorKind {
+    ParseIntError(ParseIntError),
+    TryFromSliceError(TryFromSliceError),
+    InvalidLenError,
+}
+
+impl Display for ParseGuidErrorKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseGuidErrorKind::ParseIntError(x) => {
+                write!(f, "{}", x)
+            }
+            ParseGuidErrorKind::TryFromSliceError(x) => {
+                write!(f, "{}", x)
+            }
+            ParseGuidErrorKind::InvalidLenError => {
+                write!(f, "Invalid Len")
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ParseGuidError {
+    source: ParseGuidErrorKind,
+}
+
+impl Display for ParseGuidError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error: {}", self.source)
+    }
+}
+
+impl Default for ParseGuidError {
+    fn default() -> Self {
+        Self {
+            source: ParseGuidErrorKind::InvalidLenError,
+        }
+    }
+}
+
+impl std::error::Error for ParseGuidError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match (*self).source {
+            ParseGuidErrorKind::ParseIntError(ref x) => Some(x),
+            ParseGuidErrorKind::TryFromSliceError(ref x) => Some(x),
+            ParseGuidErrorKind::InvalidLenError => None,
+        }
+    }
+}
+
+impl ParseGuidError {
+    fn parse_int_err(x: ParseIntError) -> ParseGuidError {
+        Self {
+            source: ParseGuidErrorKind::ParseIntError(x),
+        }
+    }
+    fn try_from_slice_err(x: TryFromSliceError) -> ParseGuidError {
+        Self {
+            source: ParseGuidErrorKind::TryFromSliceError(x),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn from_str_test() {
+        let guid: Guid = "01020304-0506-0708-090a-0b0d0e0f1011".parse().unwrap();
+        assert_eq!(
+            guid,
+            Guid {
+                data1: 0x01020304,
+                data2: 0x0506,
+                data3: 0x0708,
+                data4: [0x09, 0x0a, 0x0b, 0x0d, 0x0e, 0x0f, 0x10, 0x11],
+            },
+        );
     }
 }
